d5e006bf1d23cd8f0df5ffaf5e00e3b1d9bb45e5
Maksim Golov <m.golov@activevideo.com>
2017-02-04 22:04:20 +0100

    Added overF

--- Lenses.hs
+++ Lenses.hs
@@ -1,3 +1,6 @@
+
+{-# LANGUAGE RankNTypes #-}
+
 -- http://www.seas.upenn.edu/~cis194/fall16/lectures/14-lenses.html
 
 data Atom = Atom { _element :: String, _point :: Point } deriving Show
@@ -23,6 +26,7 @@ setAtomX x a = setPoint (setX x (_point a)) a
 
 data Lens a b = Lens { view :: a -> b
                      , over :: (b -> b) -> (a -> a)
+                     , overF :: forall t. Functor t => (b -> t b) -> (a -> t a)
                      }
 
 point :: Lens Atom Point
@@ -38,12 +42,15 @@ setAtomX' :: Double -> Atom -> Atom
 setAtomX' = set (point `comp` x)
 
 mkLens :: (a -> b) -> (b -> a -> a) -> Lens a b
-mkLens view set = Lens view over
-    where over f a = set (f (view a)) a
+mkLens view set = Lens view over overF
+    where
+        over f a = set (f (view a)) a
+        overF f a = (\b' -> set b' a) <$> f (view a)
 
 comp :: Lens a b -> Lens b c -> Lens a c
 comp l1 l2 = Lens (view l2 . view l1)
                   (over l1 . over l2)
+                  (overF l1 . overF l2)
 
 set :: Lens a b -> b -> a -> a
 set l x = over l (const x)
