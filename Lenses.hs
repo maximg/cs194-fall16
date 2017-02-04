d7bf1b8474cafa4ce0d940ea19ed6d5472bf51b2
Maksim Golov <m.golov@activevideo.com>
2017-02-04 13:46:32 +0100

    Lenses, part 1

---
+++ Lenses.hs
@@ -0,0 +1,47 @@
+-- http://www.seas.upenn.edu/~cis194/fall16/lectures/14-lenses.html
+
+data Atom = Atom { _element :: String, _point :: Point } deriving Show
+data Point = Point { _x :: Double, _y :: Double } deriving Show
+
+at = Atom "atom" (Point 55 66)
+pt = Point 77 88
+
+setPoint :: Point -> Atom -> Atom
+setPoint p a = a { _point = p }
+setElement :: String -> Atom -> Atom
+setElement e a = a { _element = e }
+setX, setY:: Double -> Point -> Point
+setX x p = p { _x = x }
+setY y p = p { _y = y }
+
+
+getAtomX :: Atom -> Double
+getAtomX = _x . _point
+
+setAtomX :: Double -> Atom -> Atom
+setAtomX x a = setPoint (setX x (_point a)) a
+
+data Lens a b = Lens { view :: a -> b
+                     , set :: b -> a -> a
+                     }
+
+point :: Lens Atom Point
+point = Lens _point setPoint
+element :: Lens Atom String
+element = Lens _element setElement
+x, y :: Lens Point Double
+x = Lens _x setX
+y = Lens _y setY
+
+comp :: Lens a b -> Lens b c -> Lens a c
+-- comp (Lens v1 s1) (Lens v2 s2) = Lens (v2 . v1) (\x y -> s1 (s2 x (v1 y)) y)
+comp l1 l2 = Lens (view l2 . view l1) (\x y -> set l1 (set l2 x (view l1 y)) y)
+
+setAtomX' :: Double -> Atom -> Atom
+setAtomX' = set (point `comp` x)
+
+over :: Lens a b -> (b -> b) -> (a -> a)
+over l f x = set l (f (view l x)) x
+
+comp' :: Lens a b -> Lens b c -> Lens a c
+comp' l1 l2 = Lens (view l2 . view l1) (\c -> over l1 (set l2 c))
